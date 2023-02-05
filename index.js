91d14d76da26a2d184276b8e899bf0fa037a1e23
dedayorr <dedayo16@gmail.com>
2023-02-05 01:21:41 +0100

    updated

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const hamburger = document.getElementsByClassName("hamburger")[0]
 const menuBar = document.getElementById("menubar")
+const list = document.querySelectorAll('li')
+const items = document.getElementsByClassName('items')[0]
 // console.log(hamburger)
 hamburger.addEventListener('click', ()=>{
     menuBar.classList.toggle("hidden")
+})
+
+list.addEventListener('click', ()=>{
+items.classList.display = ""
 })
